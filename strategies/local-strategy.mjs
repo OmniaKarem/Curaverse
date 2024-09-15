09460154b62e6437ae3349e13ea4a8735c7ffb86
Omnia Karem <125597216+OmniaKarem@users.noreply.github.com>
2024-09-15 18:22:57 +0300

    Add files via upload

---
+++ strategies/local-strategy.mjs
@@ -0,0 +1,43 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { User } from "../utils/db.mjs";
+
+export default passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+          return done(null, false, { message: "Email is not correct" });
+        }
+
+        if (user.password !== password) {
+          return done(null, false, { message: "Password is not correct" });
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.user_id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findOne({ where: { user_id: id } });
+    if (!user) {
+      return done(new Error("User not found"), null);
+    }
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
